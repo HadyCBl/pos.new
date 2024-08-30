4b203296cfeb3a7237ad62a8700a4da406722d48
Harvey-MSI <chenbolh@gmail.com>
2024-08-30 17:19:57 -0600

    integracion pwa

--- pages/index.php
+++ pages/index.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/x-icon" href="../assets/img/microsystem.png">
     <title>Pagos</title>
     <link href="../dist/output.css" rel="stylesheet">
 </head>
